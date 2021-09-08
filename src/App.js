620a366fa0861d7b90277061558c8c58aab289d9
Suresh <kannurisuresh2@gmail.com>
2021-09-08 15:03:13 +0000

    adds a some text

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import './App.css'
 
 class App extends Component {
   state = {
-    isLoggedIn: false,
-  }
-
-  renderAuthButton = () => {
-    const {isLoggedIn} = this.state
-    if (isLoggedIn === true) {
-      return <button type="button">Logout</button>
-    }
-    return <button type="button">Login</button>
+    isLoggedIn: true,
   }
 
   render() {
+    const {isLoggedIn} = this.state
+    let authButton
+    if (isLoggedIn === true) {
+      authButton = <button type="button">Logout</button>
+    } else {
+      authButton = <button type="button">Login</button>
+    }
+
     return (
       <div className="container">
         <Welcome greeting="Hello" name="User" />
-        {this.renderAuthButton()}
+        {authButton}
       </div>
     )
   }
